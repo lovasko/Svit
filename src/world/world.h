22ad210993e09bb8b219355ffa1ce8d3d7285a5c
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-08 17:13:55 +0200

    Added Lights to the World

--- src/world/world.h
+++ src/world/world.h
@@ -3,13 +3,20 @@
 
 #include "camera/camera.h"
 #include "node/node.h"
+#include "light/light.h"
 
+#include <vector>
+
+
+
+#include <memory>
 namespace Svit
 {
 	struct World
 	{
 		Node *scene;
 		Camera *camera;
+		std::vector<std::unique_ptr<Light>> lights;
 	};
 }
 
