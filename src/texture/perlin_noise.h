9e7cc7ae72a7dff0fa0593938c7791b692fb27a0
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-08 03:17:07 +0200

    Added Perlin noise texture

---
+++ src/texture/perlin_noise.h
@@ -0,0 +1,37 @@
+#ifndef SVIT_PERLIN_NOISE_TEXTURE
+#define SVIT_PERLIN_NOISE_TEXTURE
+
+#include "texture/texture.h"
+
+#include <vector>
+
+namespace Svit
+{
+	class PerlinNoiseTexture : public Texture
+	{
+		private:
+			Vector3 black;
+			Vector3 white;
+			unsigned int octave_count;
+			std::vector<float> amplitudes;
+			std::vector<float> frequencies;
+
+			float
+			noise (int _x, int _y, int _z);
+
+			float
+			noise_3D (Point3 _point);
+
+		public:
+			PerlinNoiseTexture (Vector3 _black, Vector3 _white);
+
+			void
+			add_octave (float _amplitude, float _frequency);
+
+			virtual Vector3
+			get_color (Point3 _point);
+	};
+}
+
+#endif
+
