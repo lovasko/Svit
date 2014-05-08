ed29a5c545abebabdb3034d87873f5f65d3ec2e6
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-08 03:17:23 +0200

    Added wood variant of the Perlin noise texture

---
+++ src/texture/wood_perlin_noise.cpp
@@ -0,0 +1,24 @@
+#include "texture/wood_perlin_noise.h"
+
+namespace Svit
+{
+	WoodPerlinNoiseTexture::WoodPerlinNoiseTexture (float _param)
+	{
+		param = _param;
+	}
+
+	Vector3
+	WoodPerlinNoiseTexture::get_color (Point3 _point)
+	{
+		float sum = 0.0;
+		float discard;
+		for (unsigned int i = 0; i < octave_count; i++)
+			sum += modff(noise_3D(_point * frequencies[i]) * amplitudes[i] * _param,
+			    &discard);
+
+		sum = (sum + 1.0f) / 2.0f;
+
+		return lerp_1D<float>(black, white, sum);
+	}
+}
+
---
+++ src/texture/wood_perlin_noise.h
@@ -0,0 +1,22 @@
+#ifndef SVIT_WOOD_PERLIN_NOISE_TEXTURE
+#define SVIT_WOOD_PERLIN_NOISE_TEXTURE
+
+#include "texture/perlin_noise.h"
+
+namespace Svit
+{
+	class WoodPerlinNoiseTexture : public PerlinNoiseTexture
+	{
+		private: 
+			float param;
+
+		public:
+			WoodPerlinNoiseTexture (float _param = 20.0);	
+
+			Vector3
+			get_color (Point3 _point);
+	}
+}
+
+#endif
+
