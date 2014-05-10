a2da390c1c0ead5f425c5f19351516c72e5b28d9
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-10 21:39:26 +0200

    Changed the Point implementation
    From now on, Point is only another name for the Vector type. This is done with
    intention to simplify the operations.h and not to have duplicate code.

--- src/geom/point.h
+++ src/geom/point.h
@@ -1,77 +1,15 @@
 #ifndef SVIT_POINT
 #define SVIT_POINT
 
-#include <iostream>
-#include <iomanip>
-#include <stdexcept>
-#include <assert.h>
+#include "geom/vector.h"
 
 namespace Svit
 {
-	template <typename Scalar>
-	class Point
-	{
-		public:
-			Point () 
-				: x(0), y(0), z(0), w(0)
-			{}
-
-			Point (Scalar _x, Scalar _y)
-				: x(_x), y(_y), z(0), w(0)
-			{ }
-
-			Point (Scalar _x, Scalar _y, Scalar _z)
-				: x(_x), y(_y), z(_z), w(0)
-			{ }
-
-			Point (Scalar _x, Scalar _y, Scalar _z, Scalar _w)
-				: x(_x), y(_y), z(_z), w(_w)
-			{ }
-
-			void
-			dump (const char *name, unsigned int level = 0)
-			{
-				std::string indentation(level*2, ' ');
-				std::cout << std::setprecision(10) << std::fixed;
-				std::cout << indentation << name << " = Point (" << x << ", " << y <<
-				    ", " << z << ", " << w << ")" << std::endl;
-			}
-
-			Scalar& 
-			operator[](const int nth)
-			{
-				assert(nth >= 0 && nth <= 3);
-				switch (nth)
-				{
-					case 0:
-						return x;
-
-					case 1:
-						return y;
-
-					case 2:
-						return z;
-
-					case 3:
-						return w;
-				}
-
-				throw std::runtime_error("Bad index");
-			}
-
-			Scalar x, y, z, w;
-	};
-
-	typedef Point<float> Point4;
-	typedef Point<float> Point3;
-	typedef Point<float> Point2;
-	typedef Point<int> Point2i;
-
+	typedef Vector<float> Point4;
+	typedef Vector<float> Point3;
+	typedef Vector<float> Point2;
+	typedef Vector<int> Point2i;
 }
-
-#if defined(USE_SSE) && defined(__SSE__)
-	#include "geom/optimization/x86/point.h"
-#endif
 
 #endif
 
