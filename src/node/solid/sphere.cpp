c110563fdbb564d3d11bbc61a4f5205db471d316
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-25 00:20:56 +0200

    Adapted the Sphere to the new interface

--- src/node/solid/sphere.cpp
+++ src/node/solid/sphere.cpp
@@ -2,59 +2,64 @@
 
 namespace Svit
 {
-	std::list<Intersection>
-	Sphere::intersect (Ray& _ray)
+	boost::optional<Intersection>
+	Sphere::intersect (Ray& _ray, float _best)
 	{
-		std::list<Intersection> result;
-
 		Vector3 ray_origin_vector = _ray.origin - Point3(0,0,0);
 		Vector3 sphere_center_vector = center - Point3(0,0,0);
 
 		float A = _ray.direction % _ray.direction;
-		float B = 2.0 * (_ray.direction % ray_origin_vector) 
-		        - 2.0 * (sphere_center_vector % _ray.direction);
+		float B = 2.0f * (_ray.direction % ray_origin_vector) 
+		        - 2.0f * (sphere_center_vector % _ray.direction);
 		float C = (ray_origin_vector % ray_origin_vector)
-		        - 2.0 * (sphere_center_vector % ray_origin_vector)
+		        - 2.0f * (sphere_center_vector % ray_origin_vector)
 						+ (sphere_center_vector % sphere_center_vector)
 						- radius * radius;
 
-		float t = 1.0;
+		float t = 1.0f;
 
-		if (A == 0.0)
+		if (A == 0.0f)
 		{
 			t = - C / B;
-			
+			if (t < _best && t > 0.0f)
+			{	
+				Intersection intersection;
+				intersection.t = t;
+				intersection.point = _ray(t);
+				intersection.node = this;
+
+				boost::optional<Intersection> result(intersection);
+				return result;
+			}
+			else
+				return boost::optional<Intersection>();
+		}
+		else
+		{
+			float discriminant = B * B - 4.0f * A * C;
+
+			if (discriminant < 0.0f)
+				return boost::optional<Intersection>();
+
+			float t1 = ( - B + sqrt(discriminant)) / (2 * A);
+			float t2 = ( - B - sqrt(discriminant)) / (2 * A);
+
+			bool t1_valid = t1 < _best && t1 >= 0.0001f;
+			bool t2_valid = t2 < _best && t2 >= 0.0001f;
+
+			if (!t1_valid && !t2_valid) return boost::optional<Intersection>();
+			if ( t1_valid &&  t2_valid) t = fmin(t1, t2);
+			if (!t1_valid &&  t2_valid) t = t2;
+			if ( t1_valid && !t2_valid) t = t1;
+	
 			Intersection intersection;
 			intersection.t = t;
 			intersection.point = _ray(t);
 			intersection.node = this;
-			result.push_back(intersection);
+
+			boost::optional<Intersection> result(intersection);
+			return result;
 		}
-		else
-		{
-			float discriminant = B * B - 4.0 * A * C;
-
-			if (discriminant < 0.0)
-			{
-				return fail();
-			}
-
-			float t1 = ( - B + sqrt(discriminant)) / (2 * A);
-			float t2 = ( - B - sqrt(discriminant)) / (2 * A);
-			Intersection i1;
-			i1.t = t1;
-			i1.point = _ray(t1);
-			i1.node = this;
-			result.push_back(i1);
-
-			Intersection i2;
-			i2.t = t2;
-			i2.point = _ray(t2);
-			i2.node = this;
-			result.push_back(i2);
-		}
-
-		return result;
 	}
 
 	void
