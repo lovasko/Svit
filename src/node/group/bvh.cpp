5c91e0d76099c9b17c9a898dc9db708c6d42e2cf
Daniel Lovasko <lovasko@freebsd.org>
2015-04-04 04:45:34 +0100

    Initial BVH implementation

---
+++ src/node/group/bvh.cpp
@@ -0,0 +1,223 @@
+#include <iterator>
+#include <queue>
+#include <stack>
+#include <utility>
+
+#include "node/group/bvh.h"
+
+namespace Svit
+{
+	BVH::BVH (AxisSelector* _axis_selector,
+	          SplitStrategy* _split_strategy,
+	          unsigned int _max_bucket_size,
+	          unsigned int _max_depth)
+	: axis_selector(_axis_selector),
+	  split_strategy(_split_strategy),
+	  max_bucket_size(_max_bucket_size),
+	  max_depth(_max_depth)
+	{
+	}
+
+	boost::optional<Intersection>
+	BVH::intersect (Ray& _ray, float _best)
+	{
+		std::stack<BVHNode*> s;
+		s.push(&root);
+
+		bool found = false;
+		float best = _best;
+		Intersection best_intersection;
+
+		while (!s.empty())
+		{
+			BVHNode* bvhnode = s.top();
+			s.pop();
+
+			if (!bvhnode->bounding_box.intersect(_ray))
+				continue;
+
+			if (bvhnode->is_leaf)
+			{
+				for (unsigned int i = bvhnode->left_idx; i < bvhnode->right_idx; i++)
+				{
+					auto isect = nodes[i]->intersect(_ray, best);
+					if (isect)
+					{
+						found = true;
+						best = isect->t;
+						best_intersection = *isect;
+					}
+				}
+			}
+			else
+			{
+				if (bvhnode->left != nullptr)
+					s.push(bvhnode->left);
+
+				if (bvhnode->right != nullptr)
+					s.push(bvhnode->right);
+			}
+		}
+    
+		if (found)
+			return boost::optional<Intersection>(best_intersection);
+		else
+			return boost::optional<Intersection>();  
+	}
+
+	void
+	BVH::add (Node* _node)
+	{
+		nodes.push_back(_node);
+		bounding_box.extend(_node->bounding_box);
+	}
+
+	void
+	BVH::init_inner_node (BVHNode* _bvhnode,
+	                      Axis _parent_split_axis,
+	                      unsigned int _left_idx,
+	                      unsigned int _right_idx)
+	{
+		_bvhnode->is_leaf = false;
+
+		BoundingBox box;
+		for (unsigned int i = _left_idx; i < _right_idx; i++)
+			box.extend(nodes[i]->bounding_box);
+		_bvhnode->bounding_box = box;
+
+		_bvhnode->split_axis = axis_selector->next(_parent_split_axis, box);
+	}
+
+	void
+	BVH::build (unsigned int _left_idx,
+	            unsigned int _right_idx,
+	            BVHNode* _bvhnode,
+	            unsigned int _depth)
+	{
+		if (_right_idx - _left_idx <= max_bucket_size || _depth >= max_depth)
+		{
+			_bvhnode->is_leaf = true;
+			_bvhnode->left_idx = _left_idx;
+			_bvhnode->right_idx = _right_idx;
+			_bvhnode->left = nullptr;
+			_bvhnode->right = nullptr;
+		}
+		else
+		{
+			float split_coord = split_strategy->get_coordinate(_bvhnode->split_axis, 
+			                                                   _bvhnode->bounding_box);
+			Axis split_axis = _bvhnode->split_axis;
+
+			auto split_it = std::partition(nodes.begin() + _left_idx,
+			                               nodes.begin() + (_right_idx-1),
+				[&split_coord, &split_axis](Node* __node) -> bool
+				{
+					return (__node->bounding_box.centroid()[split_axis] < split_coord);
+				});
+
+			unsigned int split_idx = std::distance(nodes.begin(), split_it);
+
+			if (split_idx != _left_idx)
+			{
+				BVHNode* left_node = new BVHNode();
+				left_node->left = left_node->right = nullptr;
+
+				init_inner_node(left_node, _bvhnode->split_axis, _left_idx, split_idx);
+				_bvhnode->left = left_node;
+				build(_left_idx, split_idx, left_node, _depth+1);
+			}
+
+			if (split_idx != _right_idx)
+			{
+				BVHNode* right_node = new BVHNode();
+				right_node->left = right_node->right = nullptr;
+
+				init_inner_node(right_node, _bvhnode->split_axis, split_idx, _right_idx);
+				_bvhnode->right = right_node;
+				build(split_idx, _right_idx, right_node, _depth+1);
+			}
+		}
+	}
+
+	void
+	BVH::finish ()
+	{
+		root.is_leaf = false;
+		root.bounding_box = bounding_box;
+		root.split_axis = axis_selector->next(Axis::Z, bounding_box);
+
+		build(0, nodes.size(), &root, 0);
+		cut();
+	}
+
+	void
+	BVH::cut ()
+	{
+		std::stack<BVHNode*> s;
+
+		/* TODO root might be one-childed */
+		s.push(&root);
+
+		while (!s.empty())
+		{
+			auto bvhnode = s.top();
+			s.pop();
+
+			/* TODO do deleting! */
+
+			if (bvhnode->left->left == nullptr)
+				bvhnode->left = bvhnode->left->right;
+
+			if (bvhnode->left->right == nullptr)
+				bvhnode->left = bvhnode->left->left;
+
+			if (bvhnode->right->right == nullptr)
+				bvhnode->right = bvhnode->right->left;
+
+			if (bvhnode->right->left == nullptr)
+				bvhnode->right = bvhnode->right->right;
+		}
+	}
+
+	void
+	BVH::dump (std::string _name)
+	{
+		/* queue entry */
+		struct qe
+		{
+			BVHNode* bvhnode;
+			unsigned int level;
+
+			qe (BVHNode* _bvhnode, unsigned int _level)
+			{
+				bvhnode = _bvhnode;
+				level = _level;
+			}
+		};
+
+		std::stack<struct qe*> q;
+		q.push(new qe(&root, 0));
+
+		while (!q.empty())
+		{
+			struct qe* e = q.top();
+			q.pop();
+
+			BVHNode* bvhnode = e->bvhnode;
+			unsigned int level = e->level;
+
+			std::string indent(level*2, ' ');
+			std::cout << indent 
+			          << "* "
+			          << (bvhnode->is_leaf ? std::to_string(bvhnode->right_idx - bvhnode->left_idx) : std::string(""))
+			          << std::endl;
+
+			if (bvhnode->left != nullptr)
+				q.push(new qe(bvhnode->left, level+1));
+
+			if (bvhnode->right != nullptr)
+				q.push(new qe(bvhnode->right, level+1));
+		}
+	}
+}
+
---
+++ src/node/group/bvh.h
@@ -0,0 +1,73 @@
+#ifndef SVIT_BVH_GROUP
+#define SVIT_BVH_GROUP
+
+#include <vector>
+
+#include "geom/axis.h"
+#include "geom/bounding_box.h"
+#include "node/group/axis_selector/axis_selector.h"
+#include "node/group/group.h"
+#include "node/group/split_strategy/split_strategy.h"
+#include "node/node.h"
+
+namespace Svit
+{
+	struct BVHNode
+	{
+		bool is_leaf;
+		unsigned int left_idx;
+		unsigned int right_idx;
+		BVHNode* left;
+		BVHNode* right;
+		Axis split_axis;
+		BoundingBox bounding_box;
+	};
+
+	class BVH : public Group
+	{
+		private:
+			std::vector<Node*> nodes;
+			BVHNode root;
+
+			AxisSelector* axis_selector;
+			SplitStrategy* split_strategy;
+			unsigned int max_bucket_size;
+			unsigned int max_depth;
+
+			void
+			build (unsigned int _left_idx,
+			       unsigned int _right_idx,
+			       BVHNode* _bvhnode,
+			       unsigned int _depth);
+
+			void
+			init_inner_node (BVHNode* _bvhnode,
+			                 Axis _parent_split_axis,
+			                 unsigned int _offset,
+			                 unsigned int _length);
+
+			void
+			cut ();
+
+		public:
+			BVH (AxisSelector* _axis_selector,
+			     SplitStrategy* _split_strategy,
+			     unsigned int _max_bucket_size,
+			     unsigned int _max_depth);
+
+			boost::optional<Intersection>
+			intersect (Ray& _ray, float _best) override;
+
+			void
+			add (Node* _node) override;
+
+			void
+			finish () override;
+
+			void
+			dump (std::string _name);
+	};
+}
+
+#endif
+
